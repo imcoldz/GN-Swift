4c02dcdf3184ae04121a258973c065026a45d0fd
daneozxy <daneozxy@yahoo-inc.com>
2015-01-20 18:38:12 -0800

    push finished in-course project

---
+++ GoolgeNow/TryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TryViewController.swift
+//  GoolgeNow
+//
+//  Created by Xiangyu Zhang on 1/20/15.
+//  Copyright (c) 2015 Yahoo!. All rights reserved.
+//
+
+import UIKit
+
+class TryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
